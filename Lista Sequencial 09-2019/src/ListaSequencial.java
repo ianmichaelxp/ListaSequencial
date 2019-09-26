99d346acfb1b9828114f7dde05feafef23da9a3f
Ian Michael <37078140+ianmichaelxp@users.noreply.github.com>
2019-09-26 12:21:35 -0300

    Mudanças nos metodos inicio e fim

--- Lista Sequencial 09-2019/src/ListaSequencial.java
+++ Lista Sequencial 09-2019/src/ListaSequencial.java
@@ -3,44 +3,56 @@ public class ListaSequencial {
 	private static final int ALOCACAO_INICIAL = 3;
 	private Object[] arrayInterno = new Object[ALOCACAO_INICIAL];
 	private int inseridos;
-	int pos = 0;
 	
-	public void adicionarInicio(Object item)
+	public void adicionarFim(Object item)
 	{
-		if(inseridos == 0) {
-			arrayInterno[0] = item;
-			pos++;
-			inseridos++;
-		}
 		
-		else if(inseridos == arrayInterno.length)
+		if(inseridos == arrayInterno.length)
+			{
+				dobrarArray();
+			}
+		else 
+			{	
+				arrayInterno[inseridos] = item;
+				inseridos++;
+			}
+	}
+	
+	public void adicionarInicio(Object item) 
+	{
+		if(inseridos == arrayInterno.length) 
 		{
-			Object[] novoArray = new Object[inseridos * 2];
-			for(int i = 0; inseridos < arrayInterno.length; i++) 
+			dobrarArray();
+		}
+		else 
+		{
+			Object[] novoArray = new Object[arrayInterno.length];
+			for(int i = 0;i < inseridos;i++) 
 			{
 				novoArray[i+1] = arrayInterno[i];
-			}	
-			arrayInterno = novoArray;
-		}
-		else {
-			for(int i = 0; i < inseridos; i++) {
-				if(inseridos <= arrayInterno.length) 
-				{
-					arrayInterno[pos] = arrayInterno[pos-1];
-					pos++;
-					
-				}
-				
+				i++;
 			}
-			arrayInterno[0] = item;
-			inseridos++;
-			pos = 0;
-		}
+			arrayInterno = novoArray;
+		}		
+		arrayInterno[0] = item;
+		inseridos++;
+	}
+	
+	public void dobrarArray() 
+	{	
+		Object[] novoArray = new Object[inseridos * 2];
+		for(int i = 0; i < arrayInterno.length; i++) 
+		{
+			novoArray[i] = arrayInterno[i];
+		}	
+		arrayInterno = novoArray;
 		
 	}
 	
-	public Object retorna(int posicao) {
+	public Object retorna(int posicao) 
+	{
 		return arrayInterno[posicao];
 	}
+	
 
 }
